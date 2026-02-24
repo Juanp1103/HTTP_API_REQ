faf1f9e7050d0f819f13735f21587d5874f85a9f
Juanp1103 <juanp.hernandezv@upb.edu.co>
2026-02-23 23:31:34 -0500

    initial commit

---
+++ SID-HTTP/Assets/Script/HTTP.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Http : MonoBehaviour
+{
+    [SerializeField]
+    private int characterId = 12;
+    [SerializeField]
+    private string URL = "https://rickandmortyapi.com/api/character";
+    [SerializeField]
+    private RawImage CharacterImage;
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(URL + "/" + characterId);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+            if (www.responseCode == 404)
+            {
+                Debug.Log("Character not found");
+            }
+        }
+        else
+        {
+            Character character = JsonUtility.FromJson<Character>(www.downloadHandler.text);
+            Debug.Log(character.name + " is a " + character.species);
+
+            StartCoroutine(GetTexture(character.image));
+
+
+        }
+    }
+
+    IEnumerator GetTexture(string imageUrl)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(imageUrl))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                var texture = DownloadHandlerTexture.GetContent(uwr);
+                CharacterImage.texture = texture;
+            }
+        }
+    }
+}
+class Character
+{
+    public int id;
+    public string name;
+    public string species;
+    public string image;
+
+}
+
+
+
